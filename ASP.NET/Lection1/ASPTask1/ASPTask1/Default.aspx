fef343ca53386a1bb1b5a09c22cd2be760d90ebf
Stas <stas249501@gmail.com>
2015-05-15 22:57:42 +0300

    start work with asp.net

---
+++ ASP.NET/Lection1/ASPTask1/ASPTask1/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ASPTask1.Default" Trace="true" TraceMode="SortByTime" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <hr/>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Table ID="Table1" runat="server" BorderStyle="Double" CellPadding="5" GridLines="Both">
+            <asp:TableRow runat="server" HorizontalAlign="Center">
+                <asp:TableCell runat="server" HorizontalAlign="Center">Keys</asp:TableCell>
+                <asp:TableCell runat="server" HorizontalAlign="Center">Values</asp:TableCell>
+            </asp:TableRow>
+        </asp:Table>
+        <hr/>
+        <asp:Button ID="Button1" runat="server" Text="Отправить запрос" OnClick="Button1_OnClick"/>
+    </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET/Lection1/ASPTask1/ASPTask1/Default.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPTask1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            log("Page_PreInit");
+        }
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            log("Page_Init");
+        }
+
+        protected void Page_InitComplete(object sender, EventArgs e)
+        {
+            log("Page_InitComplete");
+        }
+
+        protected void Page_PreLoad(object sender, EventArgs e)
+        {
+            log("Page_PreLoad");
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            log("Page_Load");
+            //Page.EnableEventValidation = false;
+            Page.EnableViewState = false;
+        }
+
+        protected void Page_LoadComplete(object sender, EventArgs e)
+        {
+            log("Page_LoadComplete");
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            log("Page_PreRender");
+        }
+
+        protected void Page_PreRenderComplete(object sender, EventArgs e)
+        {
+            log("Page_PreRenderComplete");
+        }
+
+        protected void Page_SaveStateComplete(object sender, EventArgs e)
+        {
+            log("Page_SaveStateComplete");
+        }
+        
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            try
+            {
+                log("Page_Unload");
+            }
+            catch (HttpException)
+            {
+                // Обьект Response уже перестал быть доступен, так как ответ отправился пользователю.
+                // Поэтому невозможно залогировать вызов этого метода.
+                // Обрабатывать это событие необходимо для освобождения ресурсов.
+            }
+        }
+
+        protected void Button1_OnClick(object sender, EventArgs e)
+        {
+            // Page.Request.Form содержит коллекцию переменных формы, обратно
+            // отправляемых странице.
+
+            foreach (string i in Page.Request.Form.Keys)
+            {
+                addCells(i, Page.Request.Form[i]);
+            }
+        }
+
+        private void addCells(string v1, string v2)
+        {
+            var row = new TableRow();
+            row.Cells.AddRange(new[] { new TableCell() { Text = v1 }, new TableCell() { Text = v2 } });
+            Table1.Rows.Add(row);
+        }
+
+        private void log(string message)
+        {
+            Page.Response.Write(string.Format("{0} {1}<br/>", message, DateTime.Now.ToString("G")));
+        }
+    }
+}
