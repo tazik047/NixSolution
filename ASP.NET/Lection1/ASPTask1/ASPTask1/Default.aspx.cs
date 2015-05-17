479bd9b477ca9334ac5a0ae08e1e0427316a9f28
Stas <stas249501@gmail.com>
2015-05-17 17:27:24 +0300

    change

--- ASP.NET/Lection1/ASPTask1/ASPTask1/Default.aspx.cs
+++ ASP.NET/Lection1/ASPTask1/ASPTask1/Default.aspx.cs
@@ -1,21 +1,64 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.Web;
+using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace ASPTask1
 {
     public partial class Default : System.Web.UI.Page
     {
+        protected override void Construct()
+        {
+            log("Construct");
+            base.Construct();
+        }
+
+        public override void ProcessRequest(HttpContext context)
+        {
+            log("ProcessRequest");
+            //Page.Response еще не доступен, но он доступен через свойство - context.Response
+            base.ProcessRequest(context);
+        }
+
+        protected override void InitializeCulture()
+        {
+            log("InitializeCulture");
+            base.InitializeCulture();
+        }
+
+        protected override NameValueCollection DeterminePostBackMode()
+        {
+            log("DeterminePostBackMode");
+            return base.DeterminePostBackMode();
+        }
+
+        protected override void OnPreInit(EventArgs e)
+        {
+            log("OnPreInit");
+            base.OnPreInit(e);
+        }
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            log("Page_PreInit");
+        }
+
         protected override void OnInit(EventArgs e)
         {
             log("OnInit");
             base.OnInit(e);
         }
 
-        protected override void OnLoad(EventArgs e)
+        protected void Page_Init(object sender, EventArgs e)
         {
-            log("OnLoad");
-            base.OnLoad(e);
+            log("Page_Init");
+        }
+
+        protected override void TrackViewState()
+        {
+            log("TrackViewState");
+            base.TrackViewState();
         }
 
         protected override void OnInitComplete(EventArgs e)
@@ -24,19 +67,27 @@ namespace ASPTask1
             base.OnInitComplete(e);
         }
 
-        protected void Page_PreInit(object sender, EventArgs e)
-        {
-            log("Page_PreInit");
-        }
-
-        protected void Page_Init(object sender, EventArgs e)
-        {
-            log("Page_Init");
-        }
-
         protected void Page_InitComplete(object sender, EventArgs e)
         {
             log("Page_InitComplete");
+        }
+
+        protected override object LoadPageStateFromPersistenceMedium()
+        {
+            log("LoadPageStateFromPersistenceMedium");
+            return base.LoadPageStateFromPersistenceMedium();
+        }
+
+        protected override void LoadViewState(object savedState)
+        {
+            log("LoadViewState");
+            base.LoadViewState(savedState);
+        }
+
+        protected override void OnPreLoad(EventArgs e)
+        {
+            log("OnPreLoad");
+            base.OnPreLoad(e);
         }
 
         protected void Page_PreLoad(object sender, EventArgs e)
@@ -44,9 +95,41 @@ namespace ASPTask1
             log("Page_PreLoad");
         }
 
+        protected override void OnLoad(EventArgs e)
+        {
+            log("OnLoad");
+            base.OnLoad(e);
+            if(!IsPostBack)
+                Button1_OnClick(null, e);
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             log("Page_Load");
+        }
+
+        protected override void RaisePostBackEvent(IPostBackEventHandler sourceControl, string eventArgument)
+        {
+            log("RaisePostBackEvent");
+            base.RaisePostBackEvent(sourceControl, eventArgument);
+        }
+
+        public override void Validate()
+        {
+            log("Validate");
+            base.Validate();
+        }
+
+        public override void Validate(string validationGroup)
+        {
+            log("Validate(string)");
+            base.Validate(validationGroup);
+        }
+
+        protected override void OnLoadComplete(EventArgs e)
+        {
+            log("OnLoadComplete");
+            base.OnLoadComplete(e);
         }
 
         protected void Page_LoadComplete(object sender, EventArgs e)
@@ -54,9 +137,21 @@ namespace ASPTask1
             log("Page_LoadComplete");
         }
 
+        protected override void OnPreRender(EventArgs e)
+        {
+            log("OnPreRender");
+            base.OnPreRender(e);
+        }
+
         protected void Page_PreRender(object sender, EventArgs e)
         {
             log("Page_PreRender");
+        }
+
+        protected override void OnPreRenderComplete(EventArgs e)
+        {
+            log("OnPreRenderComplete");
+            base.OnPreRenderComplete(e);
         }
 
         protected void Page_PreRenderComplete(object sender, EventArgs e)
@@ -64,36 +159,69 @@ namespace ASPTask1
             log("Page_PreRenderComplete");
         }
 
+        protected override object SaveViewState()
+        {
+            log("SaveViewState");
+            return base.SaveViewState();
+        }
+
+        protected override void SavePageStateToPersistenceMedium(object state)
+        {
+            log("SavePageStateToPersistenceMedium");
+            base.SavePageStateToPersistenceMedium(state);
+        }
+
+        protected override void OnSaveStateComplete(EventArgs e)
+        {
+            log("OnSaveStateComplete");
+            base.OnSaveStateComplete(e);
+        }
+
         protected void Page_SaveStateComplete(object sender, EventArgs e)
         {
             log("Page_SaveStateComplete");
         }
-        protected void Page_Disposed(object sender, EventArgs e)
+
+        public override void RenderControl(HtmlTextWriter writer)
         {
-            log("Page_SaveStateComplete");
+            log("RenderControl");
+            base.RenderControl(writer);
         }
+
+        protected override void Render(HtmlTextWriter writer)
+        {
+            log("Render");
+            base.Render(writer);
+        }
+
+        protected override void RenderChildren(HtmlTextWriter writer)
+        {
+            log("RenderChildren");
+            base.RenderChildren(writer);
+        }
+
+        protected override void OnUnload(EventArgs e)
+        {
+            log("OnUnload");
+            base.OnUnload(e);
+        }
+
         protected void Page_Unload(object sender, EventArgs e)
         {
-            try
-            {
-                log("Page_Unload");
-            }
-            catch (HttpException)
-            {
-                /* Обьект Response уже перестал быть доступен, так как ответ отправился пользователю.
-                 * Поэтому невозможно залогировать вызов этого метода.
-                 * Обрабатывать это событие необходимо для освобождения ресурсов.
-                 * Аналогично для события Page_Disposed не будет доступен обьект Response, так как это
-                 * событие возникает когда сборщик мусора убирает эту страницу, а в этот момент уже точно
-                 * ответ отправлен пользователю.
-                 */
-            }
+            log("Page_Unload");
+        }
+
+        public override void Dispose()
+        {
+            log("Dispose");
+            base.Dispose();
         }
 
         protected void Button1_OnClick(object sender, EventArgs e)
         {
-            /*  Page.Request.Form содержит коллекцию переменных формы, обратно
-             *  отправляемых странице.
+            /*  Page.Request.Form содержит коллекцию переменных формы.
+             *  При запуске приложения там пустая коллекция. Но при отправке
+             *  postBack запроса в эту коллекцию помещаются все элементы формы.
              *  Помимо обычных элементов формы(в данном случае Button1) эта коллекция
              *  еще содержит два ключа: __VIEWSTATE И __EVENTVALIDATION.
              *  __VIEWSTATE содержит десериализованные данные, в которых хранится состояние
@@ -119,7 +247,27 @@ namespace ASPTask1
 
         private void log(string message)
         {
-            Page.Response.Write(string.Format("{0} {1}<br/>", message, DateTime.Now.ToString("G")));
+            try
+            {
+                Page.Response.Write(string.Format("{0} {1}<br/>", message, DateTime.Now.ToString("G")));
+            }
+            catch (HttpException)
+            {
+                /* Обьект Response уже перестал быть доступен (ответ уже был отправлен пользователю)
+                 * или еще не доступен. 
+                 * Это происходит в методах\событиях: 
+                 * ProcessRequest
+                 * OnUnload
+                 * Page_OnUnload
+                 * Dispose
+                 */
+            }
+            catch (NullReferenceException)
+            {
+                /* Обьект Response еще не был создан в методе:
+                 * Construct
+                 */
+            }
         }
     }
 }
