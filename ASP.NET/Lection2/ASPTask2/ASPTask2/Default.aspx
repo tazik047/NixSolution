13b3a94291266c43b3ef369cf2e734f8fae220ff
Stas <stas249501@gmail.com>
2015-05-22 01:03:17 +0300

    start l2

---
+++ ASP.NET/Lection2/ASPTask2/ASPTask2/Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ASPTask2.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <%=Application["count"] %>
+        <asp:Button ID="Button1" runat="server" Text="Button" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET/Lection2/ASPTask2/ASPTask2/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace ASPTask2
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Application["count"] = 0;
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            int c = (int)Application["count"];
+            Debug.WriteLine(c);
+            Application["count"] = c + 1;
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+        }
+    }
+}
