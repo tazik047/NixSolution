cdd420e601a7363f2ceccde888dd6870cb108bc4
Stas <stas249501@gmail.com>
2015-06-03 16:32:03 +0300

    some fixes

--- Lection4/Task/Task/Program.cs
+++ Lection4/Task/Task/Program.cs
@@ -20,14 +20,15 @@ namespace Task
 
         static List<Employee> Deserialize()
         {
-            Stream file = null;
             try
             {
-                file = File.OpenRead("my.xml");
-                // Создаем объект для десериализации списка Employee с рутовой нодой: Employees.
-                XmlSerializer xml = new XmlSerializer(typeof(List<Employee>),
-                    new XmlRootAttribute("Employees"));
-                return (List<Employee>)xml.Deserialize(file);
+                using (var file = File.OpenRead("my.xml"))
+                {
+                    XmlSerializer xml = new XmlSerializer(typeof(List<Employee>),
+                        new XmlRootAttribute("Employees"));
+                    return (List<Employee>)xml.Deserialize(file);
+                }
+
             }
             catch (FileNotFoundException)
             {
@@ -37,25 +38,17 @@ namespace Task
             }
             catch (InvalidOperationException ex)
             {
-                Console.WriteLine("Файл имеет неправильный формат.\n" +
-                                  "Возможно вы не так назвали рутовую ноду(ее необходимо назвать Employee.)\n" +
-                                  "Возможно вы не так назвали или не указали один из элементов Employee.\n" +
-                                  "Дополнительные сведения: {0}", ex.Message);
+                Console.WriteLine(@"Файл имеет неправильный формат.
+Возможно вы не так назвали рутовую ноду(ее необходимо назвать Employee)
+Возможно вы не так назвали или не указали один из элементов Employee.
+Дополнительные сведения: {0}", ex.Message);
                 return new List<Employee>();
             }
-            finally
-            {
-                if (file != null)
-                    file.Close();
-            }
-
         }
 
         static void Print(int index, Employee e)
         {
-            // Получаем значение закрытого поля EmployeeID
             string id = getValueFromPrivateField<string>("EmployeeID", e);
-            // Получаем значение закрытого поля addres
             string address = getValueFromPrivateField<string>("address", e);
 
             Console.WriteLine("{0}, Last Name: {1}, First Name: {2}, Age: {3}, Department: {4}, Address: {5}, id: {6}",
@@ -64,7 +57,6 @@ namespace Task
 
         static List<Employee> SortEmployees(IEnumerable<Employee> emps)
         {
-            //Выбираем всех сотрудников в возрасте 25-35 и сортируем их по закрытом поля EmployeeID
             return emps.Where(e => e.Age > 24 && e.Age < 36)
                 .OrderBy(
                     e => getValueFromPrivateField<string>("EmployeeID", e))
@@ -81,7 +73,6 @@ namespace Task
         {
             using (var f = File.Create("my_new.xml"))
             {
-                // Создаем объект для сериализации списка Employee с рутовой нодой: Employees.
                 XmlSerializer xml = new XmlSerializer(typeof(List<Employee>),
                     new XmlRootAttribute("Employees"));
                 xml.Serialize(f, list);
